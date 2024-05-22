b63f6b16edf3f0562d02080279bc70c9355dbdaa
manischenko <m.anischenko@nicetu.spb.ru>
2024-05-22 11:31:46 +0300

    connect/disconnect in main window (only UI)

--- client/rabbitmq_client/widgets/main_window.py
+++ client/rabbitmq_client/widgets/main_window.py
@@ -5,16 +5,24 @@ from PyQt5.QtGui import QRegularExpressionValidator
 from ui.main_window import Ui_MainWindow
 from . import ConfigEditor
 
+from config import HOST, PORT
 
 class MainWindow(QMainWindow):
     multiply_btn_clicked = pyqtSignal(str)
+    server_connect = pyqtSignal(bool)
 
     def __init__(self, parent: QWidget | None = None) -> None:
         super().__init__(parent)
 
+        self.__is_connected = False
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(MainWindow=self)
         
+        self.ui.address_label.setText(f'{HOST}:{PORT}')
+
+        self.ui.connect_btn.clicked.connect(self.connect_server)
+
         self.ui.settings_btn.clicked.connect(self.__on_settings_btn_clicked)
         
         regex = QRegularExpression("^-?[0-9]+$")
@@ -25,8 +33,17 @@ class MainWindow(QMainWindow):
         self.ui.multiply_btn.setEnabled(False)
         self.ui.multiply_btn.clicked.connect(self.__on_multiply_btn_clicked)
 
+    def connect_server(self) -> None:
+        if self.__is_connected:
+            self.ui.connect_btn.setText("Подключиться")
+        else:
+            self.ui.connect_btn.setText("Отключиться")
+        self.__is_connected = not self.__is_connected
+        self.ui.number_line_edit.setEnabled(self.__is_connected)
+        self.ui.multiply_btn.setEnabled(self.__is_connected)
+
     def __on_settings_btn_clicked(self):
-        config_editor = ConfigEditor('rabbitmq_client/app.ini')
+        config_editor = ConfigEditor('rabbitmq_client/app.ini', 'rabbitmq_client/logger.ini')
         config_editor.show()
 
     def __on_multiply_btn_clicked(self) -> None:
