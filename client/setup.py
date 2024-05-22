96feffad3ee96b9fa646fb3a025696587377ec8c
manischenko <m.anischenko@nicetu.spb.ru>
2024-05-22 11:28:39 +0300

    automatic protomessage generation

--- client/setup.py
+++ client/setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup
+from setuptools.command.build_py import build_py
+from distutils.spawn import spawn, find_executable
+
+class Build(build_py):
+    def run(self):
+        spawn([find_executable('protoc'), '--python_out=.', 'rabbitmq_client/proto/message.proto'])
+        build_py.run(self)
+
 
 setup(
     name='rabbitmq_client',
@@ -10,4 +18,7 @@ setup(
     long_description="",
     zip_safe=False,
     packages=['rabbitmq_client'],
+    cmdclass={
+        'build_py': Build
+    },
 )
